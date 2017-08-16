48020208cd4720b0574a5e62addf9dff03eade7d
Edson Jr <eddson.jr@gmail.com>
2017-08-16 10:50:15 -0400

    [STORE/ASSETS] Classe de ImagemExercicioStore criada. Esta classe sera responsavel por gerenciar e manter estocada as imagens que irao ser mostradas em alguns exercicios. Foram tambem baixadas da internet algumas imagens de teste.
    Signed-off-by: Edson Jr <eddson.jr@gmail.com>

---
+++ Afasia/ImagemExercicioStore.swift
@@ -0,0 +1,26 @@
+//
+//  ImagemExercicioStore.swift
+//  Afasia
+//
+//  Created by Edson  Jr on 16/08/17.
+//  Copyright © 2017 Edson  Jr. All rights reserved.
+//
+// Classe de store de Imagens usadas nos exercicios
+
+
+
+import Foundation
+import UIKit
+
+
+
+class ImagemExercicioStore {
+    
+    private let dataImagemExercicio: [(     UIImage, //0 > asset da imagem
+                                            String, //1 > nome da imagem
+                                            String  //2 > letra inicial
+        )] = [
+
+             ]
+
+}
